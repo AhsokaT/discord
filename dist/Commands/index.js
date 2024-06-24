baaadda1f5643ca801c2f511dabfb2bb5c2f738e
Patrick Lynch <{ID}+{username}@users.noreply.github.com>
2024-06-24 17:11:59 +0100

    testing npx tsx

--- dist/Commands/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./HousePicker");
-require("./HousePoints");
-require("./Leaderboard");
-require("./Test");
-require("./Embed");
--- dist/Handlers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./Delete");
-require("./HouseButtons");
-require("./HousePicker");
-require("./Leaderboard");
-require("./ViewChanges");
-require("./UserInfo");
-require("./HouseInfo");
