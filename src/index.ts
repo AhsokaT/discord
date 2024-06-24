f6052d19f5316f0cc092d5c24304417a78e40eb7
Patrick Lynch <{ID}+{username}@users.noreply.github.com>
2024-06-24 17:21:11 +0100

    updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { ActivityType, GatewayIntentBits } from 'discord.js';
 import { Client } from './Client/client';
-import { config } from 'dotenv';
 import './events/index';
 import './Commands/index';
 import './Handlers/index';
-
-config();
 
 const client = new Client({
     presence: {
