8331ace59958f5af17dae50d6eb874516179e41d
Patrick Lynch <{ID}+{username}@users.noreply.github.com>
2024-07-07 04:47:29 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { ActivityType, GatewayIntentBits } from 'discord.js';
 import { Client } from './client/client.js';
+import './events/load.js';
+import './commands/load.js';
+import './handlers/load.js';
 
 const client = new Client({
     presence: {
