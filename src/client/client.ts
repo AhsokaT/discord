a930b80b7af5bd08a3c8de9dc167fe4d8c0bccc2
Patrick Lynch <{ID}+{username}@users.noreply.github.com>
2024-07-09 16:10:29 +0100

    test

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,8 @@
 import { ClientOptions } from 'discord.js';
 import { DatabaseManager } from '../database/DatabaseManager.js';
 import { SapphireClient, SapphireClientOptions } from '@sapphire/framework';
+import { opendir } from 'fs/promises';
+import { join, basename } from 'path';
 
 export class Client<
     Ready extends boolean = boolean
@@ -13,8 +15,27 @@ export class Client<
         this.database = new DatabaseManager(this);
     }
 
+    async *walk(path: string): AsyncGenerator<string> {
+        const dir = await opendir(path);
+
+        for await (const dirent of dir) {
+            if (dirent.isDirectory()) yield* this.walk(join(dir.path, dirent.name));
+            else if (dirent.isFile()) yield join(dir.path, dirent.name);
+        }
+    }
+
+    async loadCommands() {
+        for await (const file of this.walk(join(process.cwd(), 'dist', 'commands'))) {
+            const module = await import(`../commands/${basename(file)}`);
+
+            console.log(module);
+        }
+    }
+
     async login(token?: string) {
         await this.database.init();
+        await this.loadCommands();
+
         return super.login(token);
     }
 }
