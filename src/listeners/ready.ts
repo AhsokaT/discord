691cda7fc7248ec39e040b8099d362d16599b9b6
Patrick Lynch <{ID}+{username}@users.noreply.github.com>
2024-07-19 22:20:19 +0100

    fix?

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,8 +1,18 @@
 import { Events, Listener } from '@sapphire/framework';
 import { Client } from '../client/client.js';
+import { ChannelId } from '../util/enum.js';
+import assert from 'assert/strict';
+import { EmbedBuilder } from 'discord.js';
 
 export class Ready extends Listener<typeof Events.ClientReady> {
     async run(ready: Client<true>) {
         console.debug(`${ready.user.tag} is online!`);
+
+        const channel = await ready.channels.fetch(ChannelId.Logs);
+
+        assert.ok(channel?.isTextBased());
+
+        const embed = new EmbedBuilder()
+            .setAuthor;
     }
 }
